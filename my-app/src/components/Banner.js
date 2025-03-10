5ce394ba88d55e5b8c2ce907ebdd0fcb0b56e839
juham <judev0227@gmail.com>
2025-03-10 22:12:01 +0900

    feat: Banner 업데이트

--- my-app/src/components/Banner.js
+++ my-app/src/components/Banner.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import axios from '../api/axios'
 import requests from '../api/request'
+import styled from 'styled-components';
 import "./Banner.css"
 
 function Banner() {
 	const [movie, setMovie] = useState([]);
+	const [isClicked, setIsClicked] = useState(false);
 
 	useEffect(() => {
 		fetchData();
@@ -31,28 +33,79 @@ function Banner() {
 		return str?.length > n ? str.substr(0, n-1) + "..." : str;
 	}
 
-	return (
-		<header className="banner" 
-			style={{
-				backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}.jpg")`,
-				backgroundPosition: "top center",
-				backgroundSize: "cover",
-				}}>
-		
+	if (!isClicked) {
+		return (
+			<header className="banner" 
+				style={{
+					backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}.jpg")`,
+					backgroundPosition: "top center",
+					backgroundSize: "cover",
+					}}>
+			
 
-			<div className="banner_contents">
-				<h1 className="banner_title">{movie.title || movie.name || movie.original_name}</h1>
+				<div className="banner_contents">
+					<h1 className="banner_title">{movie.title || movie.name || movie.original_name}</h1>
 
-				<div className="banner_buttons">
-					<button className="banner_button play">Play</button>
-					<button className="banner_button info">More Information</button>
+					<div className="banner_buttons">
+						<button 
+							className="banner_button play"
+							onClick={() => setIsClicked(!isClicked)}
+						>Play</button>
+						<button className="banner_button info">More Information</button>
+					</div>
 				</div>
-			</div>
 
-			<h2 className="banner_description">{truncate(movie.overview, 100)}</h2>
+				<h2 className="banner_description">{truncate(movie.overview, 100)}</h2>
 
-		</header>
-	)
+			</header>
+		)
+	} else {
+		return (
+			<Container>
+				<HomeContainer>
+					<Iframe
+						src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?autoplay=1&mute=1&controls=0&loop=1&playlist=${movie.videos.results[0].key}`}
+						width="640"
+						height="360"
+						frameBorder="0"
+						allow="autoplay; fullscreen"
+					></Iframe>
+				</HomeContainer>
+			</Container>
+		)
+	}
 }
 
 export default Banner
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	height: 100vh;
+	background-color: #111;
+	color: white;
+`;
+
+const HomeContainer = styled.div`
+	width: 100%;
+	height: 100vh;
+`;
+
+const Iframe = styled.iframe`
+	width: 100%;
+	height: 100vh;
+	border: none;
+	z-index: -1;
+	
+	&:after {
+		content: "";
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+	}
+`
