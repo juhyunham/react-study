c62ee6d33779b5434863def131c661f6b14ec579
juham <judev0227@gmail.com>
2025-03-12 09:20:33 +0900

    feat: 스와이퍼 적용

--- my-app/src/components/Row.js
+++ my-app/src/components/Row.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import axios from '../api/axios'
-import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid'
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from 'swiper/modules';
 import "./Row.css"
 
 export default function Row({title, id, fetchUrl, isLargeRow}) {
@@ -16,40 +18,62 @@ export default function Row({title, id, fetchUrl, isLargeRow}) {
 		setMovies(request.data.results)
 	}
 
-	console.log(`movie`, movies)
-
 	return (
 		<section>
 			<h2 className="slideTitle">{title}</h2>
 			<div className="slider">
-				<ChevronLeftIcon
+				<div
 					className="arrowLeft"
-					width="50"
-					height="50"
-				/>
-				<ul className="slideList">
+				>
+					<ChevronLeftIcon
+						width="50"
+						height="50"
+					/>
+				</div>
+				<Swiper
+					className="slideList"
+					modules={[Navigation]}
+					navigation={{ nextEl: ".arrowRight", prevEl: ".arrowLeft" }} 
+					spaceBetween={15} 
+					loop={false} 
+					watchOverflow={true} 
+					autoWidth={false}
+					slidesPerView={"auto"}
+					breakpoints={{ 
+						320: { slidesPerView: 2.5 }, 
+						768: { slidesPerView: 5 }, 
+						1024: { slidesPerView: 6 }, 
+					}}
+					//style={{ width: "100%" }} 
+					>
 					{movies?.map((movie) => {
 						return (
-							<li 
+							<SwiperSlide
 								key={movie.id}
-								className="slideItem">
+								className="slideItem"
+								style={{ width: "150px" }}
+							>
 								<img
 									className={`poster "${isLargeRow && 'posterLarge'}"`}
 									src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
 									alt={movie.name}
 								/>
 								<div>
-									<h4>{movie.name}</h4>
+									<h4 className="movieTitle">{movie.name}</h4>
 								</div>
-							</li>
+							</SwiperSlide>
 						)
 					})}
-				</ul>
-				<ChevronRightIcon 
+				</Swiper>
+				<div
 					className="arrowRight"
-					width="50"
-					height="50"
-				/>
+				>
+					<ChevronRightIcon 
+						width="50"
+						height="50"
+					/>
+				</div>
+				
 			</div>
 		</section>
 	)
